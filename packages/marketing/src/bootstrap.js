d15ef7364c21d12c308e21a459882d28ef24fee7
Nick Olson <naolson.1819@gmail.com>
2023-02-02 16:53:45 -0600

    created routing

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  // this history is for routing inside of marketing app
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  // this object gets sent to the container
+  // then we can use the callback so the container can commuicate
+  // the browser url path to the marketing app
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    }
+  }
 }
 
 // situation 1
@@ -13,7 +34,8 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    // Use browser history when in development, instead of memory history
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
